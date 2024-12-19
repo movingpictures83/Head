79baf78c9af80d46f44b06379aef427da1c52de3
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-12-19 18:02:04 -0500

    Initial

---
+++ HeadPlugin.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+source("RIO.R")
+
+input <- function(inputfile) {
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <<- paste(pfix, "/", sep="")
+  }
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+    pbmc <- readRDS(paste(pfix, parameters["rdsfile", 2], sep="/"))
+topN <- head(VariableFeatures(pbmc), as.integer(parameters["N", 2]))
+    saveRDS(topN, outputfile)
+}
+
+
